4b38851cb0d6204b8c2fd9393d13d08ab3785f97
gvinjilian <gvinjilian2@gmail.com>
2024-10-27 19:29:07 +0400

    commit

--- Level 12/Homework/Homework.py
+++ Level 12/Homework/Homework.py
@@ -13,20 +13,20 @@ for num in range (1,101):
     elif num % 5==0:
         print(f"Buzz {num}")
 #შექმენით ორი ცვლადი, პირველში შეინახეთ 1-დან 20-ის ჩათვლით ყველა ლუწი რიცხვის ჯამი, მეორეში კი ყველა კენტის. აიყვანეთ ორივე მე-5 ხარისხში და დაპრინტეთ ის, რომელიც არის მეტი
-num = 1
-sum = 0
-while num <= 20:
-    if num % 2 != 0:  
-        sum += num
-    num += 1
-print(sum**5)
-num=0
-sum=0
-while num <=20:
-    if num % 2 ==0:
-        sum +=num
-    num=num+2
-print(sum**5)
+even_sum = 0
+odd_sum = 0
+
+for i in range(1, 21):
+    if i % 2 == 0:
+        even_sum = even_sum + i
+    else:
+        odd_sum = odd_sum + i
+
+
+if even_sum ** 5 > odd_sum ** 5:
+    print("Even sum is greater than odd sum", even_sum ** 5)
+else:
+    print("Odd sum is greater than even sum", odd_sum ** 5)
 #True or False and 5 > 3 or "name" == "name" and 123 == "123" and 5 >= 5 <--- გაიგეთ რას გამოიტანს ეს კოდი და ახსენით რატომ.
 # True      #False
 print(True or False and 5 > 3 or "name" == "name" and 123 == "123" and 5 >= 5)
