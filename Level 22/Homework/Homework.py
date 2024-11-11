eb8b0fec2485da70a7ba783c4fd2c3d62efddd2a
gvinjilian <gvinjilian2@gmail.com>
2024-11-11 23:39:57 +0400

    commit

--- Level 22/Homework/Homework.py
+++ Level 22/Homework/Homework.py
@@ -1,28 +1,27 @@
-#https://www.codewars.com/kata/523b4ff7adca849afe000035
 def greet():
     return "hello world!"
-#https://www.codewars.com/kata/50654ddff44f800200000004
+
 def multiply(a, b):
     return (a*b)
 final=multiply(2,2)
 print(final)
-#https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+
 def find_smallest_int(arr):
     smallest_int=[]
     for number in arr:
         if number < 0:
             smallest_int +=number
     return smallest_int  
-#https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+
 def solution(string):
     return string[::-1]
-#https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
 def even_or_odd(number):
     if number % 2 == 0:
         return "Even"
     else:
         return "Odd"
-#https://www.codewars.com/kata/546e2562b03326a88e000020
+
 def square_digits(num):
     return int(''.join(str(int(digit) ** 2) for digit in str(num)))
 #შექმენით ფუნქცია, რომელსაც არგუმენტად გადაეცემა რიცხვი, ხოლო ამ ფუნქციამ უნდა დაბეჭდოს 1-დან გადმოცემულ რიცხვამდე ყველა რიცხვი
